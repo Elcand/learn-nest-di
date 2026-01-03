1bf772f604f11023037dda89360652e47a1a65ae
Mikhael Candra <tjandraraharja99@gmail.com>
2026-01-03 13:43:22 +0700

    call method from other module

--- src/cpu/cpu.service.ts
+++ src/cpu/cpu.service.ts
@@ -4,4 +4,10 @@ import { PowerService } from 'src/power/power.service';
 @Injectable()
 export class CpuService {
   constructor(private powerService: PowerService) {}
+
+  compute(a: number, b: number) {
+    console.log('computing');
+    this.powerService.supplayPower(10, 'CPU');
+    return a + b;
+  }
 }
--- src/disk/disk.service.ts
+++ src/disk/disk.service.ts
@@ -1,4 +1,13 @@
 import { Injectable } from '@nestjs/common';
+import { PowerService } from 'src/power/power.service';
 
 @Injectable()
-export class DiskService {}
+export class DiskService {
+  constructor(private PowerService: PowerService) {}
+
+  getData() {
+    console.log('Reading data from disk');
+    this.PowerService.supplayPower(20, 'Disk');
+    return 'Data';
+  }
+}
